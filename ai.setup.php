0b9c2970ae4651f4f92cd939a01399663c1bd138
Abu Ayyub Al Anshary <luthfie@y7mail.com>
2021-06-02 08:01:35 +0700

    add comment to loader script

--- ai.setup.php
+++ ai.setup.php
@@ -1,15 +1,36 @@
 <?php
+/* prepare username and hostname */
 $username=basename(trim(shell_exec('whoami')));
 $hostname=basename(trim(shell_exec('hostname')));
-echo "--> {$username}@{$hostname}\r\nConnecting...\r";
-if(!function_exists('gzinflate')){
-exit("Status: [ERROR] Require zlib extension.\r\n\r\n");
-}$file='install.ai';
+
+/* prepare filename */
+$file='install.ai';
+
+/* prepare script url */
 $url='https://sabunjelly.com/ai/setup.ai';
+
+/* prepare status messages */
+$status=[
+  'Status: [ERROR] Require zlib extension.',
+  'Status: [ERROR] Failed to get script.',
+  'Status: [OK] Ready to execute script.',
+];
+
+/* print the username and hostname */
+echo "--> {$username}@{$hostname}\r\nConnecting...\r";
+
+/* checking for gz library or zlib extension */
+if(!function_exists('gzinflate')){exit("{$status[0]}\r\n\r\n");}
+
+/* get script content */
 $get=@file_get_contents($url.'?r='.mt_rand());
+
+/* inflate and save the script */
 $put=@file_put_contents($file,@gzinflate($get));
-if(!$put){
-@unlink($file);
-exit("Status: [ERROR] Failed to get script.\r\n\r\n");
-}exit("Status: [OK] Ready to execute script.\r\n\r\n");
+if(!$put){@unlink($file);exit("{$status[1]}\r\n\r\n");}
+
+/* done */
+exit("{$status[2]}\r\n\r\n");
+
+
 
