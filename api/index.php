a0729b81c7d6cc8270742d262a766ce979a4da50
9r3i <luthfie@y7mail.com>
2025-01-15 06:11:03 +0700

    change version source to v4.7.1.2455.358

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
   const version='1.1.0';
   private $mime=[];
   private $base='https://github.com/9r3i/ai/releases/download/';
-  private $aiversion='4.7.0.1913.388';
+  private $aiversion='4.7.1.2455.358';
   public function __construct(){
     return $this->s();
   }
